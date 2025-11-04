acad3cc259cac2f2969cfb50e4975dc94f053b70
OmarChoi <omarchoi80@gmail.com>
2025-11-04 13:19:31 +0900

    3. 총알 발사에 쿨타임 적용

--- Assets/02_Scripts/Player/PlayerFire.cs
+++ Assets/02_Scripts/Player/PlayerFire.cs
@@ -8,15 +8,30 @@ public class PlayerFire : MonoBehaviour
     [Header("총구")]
     public Transform[] FirePosition;
 
+    [Header("쿨타임")]
+    public float CoolTime = 0.6f;   // 몇초마다 발사가 가능한지
+    private float _elapsedTime = 0.0f;
+
     private void Update()
     {
+        _elapsedTime += Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            for (int i = 0; i < FirePosition.Length; i++)
+            // 쿨타임 확인
+            if (_elapsedTime > CoolTime)
             {
-                GameObject bullet = Instantiate(BulletPrefab);
-                bullet.transform.position = FirePosition[i].position;
+                _elapsedTime = 0.0f;
+                FireBullet();
             }
+        }
+    }
+
+    private void FireBullet()
+    {
+        for (int i = 0; i < FirePosition.Length; i++)
+        {
+            GameObject bullet = Instantiate(BulletPrefab);
+            bullet.transform.position = FirePosition[i].position;
         }
     }
 }
