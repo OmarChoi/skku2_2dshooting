45991f27a75cd28f3faf4e4454bb7b70d59b8f4a
OmarChoi <omarchoi80@gmail.com>
2025-11-04 11:18:56 +0900

    1. 가속도 적용 방식 수정

--- Assets/02_Scripts/Bullet/Bullet.cs
+++ Assets/02_Scripts/Bullet/Bullet.cs
@@ -1,13 +1,12 @@
-using System.Diagnostics;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [Header("이동속도")]
-    private float _speed = 1.0f;
     public float StartSpeed = 1.0f;
     public float EndSpeed = 7.0f;
-    public float Accelation = 1.2f;
+    public float AccelerationTime = 1.2f;
+    private float _speed = 1.0f;
 
     private void Start()
     {
@@ -16,13 +15,23 @@ public class Bullet : MonoBehaviour
 
     private void Update()
     {
-        _speed += Accelation * Time.deltaTime;
-        _speed = Mathf.Min(_speed, EndSpeed);
-        //          ㄴ 어떤 속성과 어떤 메서드를 가지고 있는지 톺아볼 필요가 있다.
+        Accelerate();
+        UpdatePosition();
+    }
 
+    private void UpdatePosition()
+    {
         Vector2 direction = Vector2.up;
         Vector2 position = transform.position;
         Vector2 newPosition = position + direction * _speed * Time.deltaTime;
         transform.position = newPosition;
+    }
+
+    private void Accelerate()
+    {
+        // AccelationTime 시간 안에 최대 속도에 도달하게 구현
+        float acceleration = ((EndSpeed - StartSpeed) / AccelerationTime);
+        _speed += acceleration * Time.deltaTime;
+        _speed = Mathf.Min(_speed, EndSpeed);
     }
 }
