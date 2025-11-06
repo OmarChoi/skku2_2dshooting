5d5e4e4a211cd97ee70e3bc7e115c0106fbe2813
OmarChoi <omarchoi80@gmail.com>
2025-11-06 15:38:16 +0900

    랜덤한 확률로 적 움직임 타입을 결정

--- Assets/02_Scripts/Enemy/EnemySpawner.cs
+++ Assets/02_Scripts/Enemy/EnemySpawner.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
+public enum EMovementType
+{
+    DirectionalMovement,
+    ChasingMovement,
+}
+
 public class EnemySpawner : MonoBehaviour
 {
     [Header("적 프리팹")]
     [SerializeField]
-    private GameObject _enemyPrefab;
+    private GameObject[] _enemyPrefab;
 
     [Header("스폰 시간")]
     [SerializeField]
@@ -14,6 +20,18 @@ public class EnemySpawner : MonoBehaviour
     [Header("스폰 쿨타임")]
     private float _minSpawnCoolTime = 1.0f;
     private float _maxSpawnCoolTime = 3.0f;
+
+    [Header("스폰 확률")]
+    private int _totalWeight = 0;
+    private int[] _probabilityWeights = new int[] { 70, 30 };
+
+    private void Start()
+    {
+        foreach (int weight in _probabilityWeights)
+        {
+            _totalWeight += weight;
+        }
+    }
 
     private void Update()
     {
@@ -36,7 +54,31 @@ public class EnemySpawner : MonoBehaviour
     private void SpawnEnemy()
     {
         ResetCoolTime();
-        GameObject enemyObject = Instantiate(_enemyPrefab, transform.position, transform.rotation);
-        enemyObject.AddComponent<ChasingMovement>();
+        EMovementType type = GetMovementType();
+        if (type == EMovementType.DirectionalMovement)
+        {
+            Instantiate(_enemyPrefab[(int)EMovementType.DirectionalMovement], transform.position, transform.rotation);
+        }
+        else if (type == EMovementType.ChasingMovement)
+        {
+            Instantiate(_enemyPrefab[(int)EMovementType.ChasingMovement], transform.position, transform.rotation);
+        }
+    }
+
+    private EMovementType GetMovementType()
+    {
+        float randomValue = UnityEngine.Random.value;
+        float totalValue = 0.0f;
+        int type = 0;
+        for (int i = 0; i < _probabilityWeights.Length; ++i)
+        {
+            totalValue += (float)_probabilityWeights[i] / _totalWeight;
+            if (randomValue < totalValue)
+            {
+                type = i;
+                break;
+            }
+        }
+        return (EMovementType)type;
     }
 }
