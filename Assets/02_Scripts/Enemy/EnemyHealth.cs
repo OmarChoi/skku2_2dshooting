a448c05c751462c5b04557d69709bb065694e53f
OmarChoi <omarchoi80@gmail.com>
2025-11-10 18:58:37 +0900

    feat: Enemy 피격시 Hit 애니메이션 실행 기능 추가
    - Animator Trigger 기반으로 Enemy 객체가 피격시 피격 애니메이션 실행 기능 추가

--- Assets/02_Scripts/Enemy/EnemyHealth.cs
+++ Assets/02_Scripts/Enemy/EnemyHealth.cs
@@ -5,9 +5,16 @@ public class EnemyHealth : MonoBehaviour
     [Header("능력치")]
     private float _health = 100.0f;
 
+    private Animator _animator = null;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     public void TakeDamage(float damage)
     {
         _health -= damage;
+        _animator.SetTrigger("Hit");
         if (_health <= 0)
         {
             Die();
