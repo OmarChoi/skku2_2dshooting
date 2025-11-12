0ae854a0cc5a0abd5642410e4a5a715f089c938a
OmarChoi <omarchoi80@gmail.com>
2025-11-12 12:44:35 +0900

    feat: 이전 점수 로드 및 점수 저장 기능 추가
    - PlayerPrefs을 사용해서 Score을 저장 및 로드 기능 구현

--- Assets/02_Scripts/Manager/ScoreManager.cs
+++ Assets/02_Scripts/Manager/ScoreManager.cs
@@ -6,9 +6,11 @@ public class ScoreManager : MonoBehaviour
     // 목표: 적을 죽일 때마다 점수를 올리고, 현재 점수를 UI에 표시하고 싶다.
     [SerializeField] private Text _currentScoreTextUI;
     private int _currentScore = 0;
+    private const string ScoreKey = "Score";
 
     private void Start()
     {
+        LoadScore();
         Refresh();
     }
 
@@ -17,10 +19,21 @@ public class ScoreManager : MonoBehaviour
         if (score <= 0) return;
         _currentScore += score;
         Refresh();
+        SaveScore();
     }
 
     private void Refresh()
     { 
         _currentScoreTextUI.text = $"현재 점수 : {_currentScore:N0}";
+    }
+
+    private void SaveScore()
+    {
+        PlayerPrefs.SetInt(ScoreKey, _currentScore);
+    }
+
+    private void LoadScore()
+    {
+        _currentScore = PlayerPrefs.GetInt(ScoreKey, 0);
     }
 }
