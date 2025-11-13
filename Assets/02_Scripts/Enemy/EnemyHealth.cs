e8b5ccdfeddd1c55709792b52faf0e5a3cd3cec4
OmarChoi <omarchoi80@gmail.com>
2025-11-13 14:15:02 +0900

    refactor: ScoreManager에 싱글톤 패턴 적용

--- Assets/02_Scripts/Enemy/EnemyHealth.cs
+++ Assets/02_Scripts/Enemy/EnemyHealth.cs
@@ -33,9 +33,7 @@ public class EnemyHealth : MonoBehaviour
             spawner.SpawnItem(transform.position);
         }
 
-        ScoreManager scoreManager = FindAnyObjectByType<ScoreManager>();
-        // todo : 추후 매직넘버 수정 예정 입니다.
-        scoreManager.AddScore(100);
+        ScoreManager.Instance.AddScore(100);
         MakeExplosionEffect();
         Destroy(gameObject);
     }
